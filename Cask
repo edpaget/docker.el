463d83faae5d3c6289dcf7f087e770ed4d24a2e6
Edward Paget <edward@cassetteta.pe>
2014-11-15 15:53:31 -0600

    Initial Commit

---
+++ Cask
@@ -0,0 +1,8 @@
+(source gnu)
+(source melpa)
+
+(package-file "docker.el")
+
+(development
+ (depends-on "ert-runner")
+ (depends-on "f"))
---
+++ docker.el
@@ -0,0 +1,36 @@
+;;; docker.el --- Emacs integration for rvm
+;;
+;; Copyright (C) 2010-2011 Edward Paget
+;; Author: Edward Paget <edward@cassetteta.pe>
+;; URL: http://www.emacswiki.org/emacs/DockerEl
+;; Version: 0.0.1
+;; Created: 15 November 2014
+;; Keywords: docker
+;; EmacsWiki: DockerEl
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; License:
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING. If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+;;; Commentary:
+;; M-x rvm-use-default prepares the current Emacs session to use
+;; the default ruby configured with rvm.
+;; M-x rvm-use allows you to switch the current session to the ruby
+;; implementation of your choice. You can also change the active gemset.
+;;; Compiler support:
+
+(provide 'docker)
+;;; docker.el ends here
